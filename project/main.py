3fe22ad0519d07563ec87712edfde10ac49f3dc5
Alekbs <aleksandrb436@gmail.com>
2023-07-16 23:26:50 +0300

    volume

--- project/main.py
+++ project/main.py
@@ -4,8 +4,8 @@ import pygame
 def main(page: Page):
     page.vertical_alignment = MainAxisAlignment.CENTER
 
-    txt_number = TextField(value="0", text_align=TextAlign.RIGHT, width=100)
-
+    #txt_number = TextField(value="0", text_align=TextAlign.RIGHT, width=100)
+    volume_slider = Slider()
     # Pick files dialog
     def pick_files_result(e: FilePickerResultEvent):
         selected_files.value = (
@@ -25,8 +25,8 @@ def main(page: Page):
     # play open file
     def play(e):
         global played
-        txt_number.value = selected_files.value
-        page.update()
+        #txt_number.value = selected_files.value
+        #page.update()
         pygame.mixer.init()
         pygame.mixer.music.load(selected_files.value)
         pygame.mixer.music.play()
@@ -36,20 +36,25 @@ def main(page: Page):
     def pause(e):
         global played
         if played == False:
-            txt_number.value = selected_files.value
-            page.update()
+            #txt_number.value = selected_files.value
+            #page.update()
             pygame.mixer.music.unpause()
             played = True
         else:
-            txt_number.value = "Pause"
-            page.update()
+            #txt_number.value = "Pause"
+            #page.update()
             pygame.mixer.music.pause()
             played = False
+
+    # chanch volume
+    def volume(e):
+        v = 0.01 * e.control.value    
+        pygame.mixer.music.set_volume(v)
 
     page.add(
         Row(
             [
-                txt_number,
+                #txt_number,
                 ElevatedButton(
                     "Pick files",
                     icon=icons.UPLOAD_FILE,
@@ -60,8 +65,10 @@ def main(page: Page):
                 selected_files,
                 IconButton(icons.PLAY_ARROW, on_click=play),
                 IconButton(icons.PAUSE, on_click=pause),
+                Slider(min=0, max=100, divisions=100, value=100, label="{value}%", on_change=volume),          
             ],
             alignment=MainAxisAlignment.CENTER,
+            
         ),
     )
 
