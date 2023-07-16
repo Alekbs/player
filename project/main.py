e7aaf6ff13187c38f46c5640a053cd1afdb58372
Alekbs <aleksandrb436@gmail.com>
2023-07-16 22:47:07 +0300

    1.0

---
+++ project/main.py
@@ -0,0 +1,70 @@
+from flet import *
+import pygame
+
+def main(page: Page):
+    page.vertical_alignment = MainAxisAlignment.CENTER
+
+    txt_number = TextField(value="0", text_align=TextAlign.RIGHT, width=100)
+
+    # Pick files dialog
+    def pick_files_result(e: FilePickerResultEvent):
+        selected_files.value = (
+            ", ".join(map(lambda f: f.path, e.files)) if e.files else "Cancelled!"
+        )
+
+        selected_files.update()
+    
+    pick_files_dialog = FilePicker(on_result=pick_files_result)
+    selected_files = Text()
+
+
+    # hide all dialogs in overlay
+    page.overlay.extend([pick_files_dialog])
+
+
+    # play open file
+    def play(e):
+        global played
+        txt_number.value = selected_files.value
+        page.update()
+        pygame.mixer.init()
+        pygame.mixer.music.load(selected_files.value)
+        pygame.mixer.music.play()
+        played= True
+
+   # pause the music
+    def pause(e):
+        global played
+        if played == False:
+            txt_number.value = selected_files.value
+            page.update()
+            pygame.mixer.music.unpause()
+            played = True
+        else:
+            txt_number.value = "Pause"
+            page.update()
+            pygame.mixer.music.pause()
+            played = False
+
+    page.add(
+        Row(
+            [
+                txt_number,
+                ElevatedButton(
+                    "Pick files",
+                    icon=icons.UPLOAD_FILE,
+                    on_click=lambda _: pick_files_dialog.pick_files(
+                        allow_multiple=True
+                    ),
+                ),
+                selected_files,
+                IconButton(icons.PLAY_ARROW, on_click=play),
+                IconButton(icons.PAUSE, on_click=pause),
+            ],
+            alignment=MainAxisAlignment.CENTER,
+        ),
+    )
+
+
+if __name__ == "__main__":
+    app(target=main)
